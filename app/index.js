7a18dca8f85d338335b08c1390a47950d10701f7
Nathan Carlson <nathan.s.carlson@gmail.com>
2017-02-11 00:52:45 -0700

    updates

---
+++ app/index.js
@@ -0,0 +1,3 @@
+import SimpleMDE from 'simplemde'
+
+var simplemde = new SimpleMDE({ element: document.getElementById("MyID") });
--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,32 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var webpack = require('webpack');
 
 module.exports = {
+    devtool: 'cheap-module-source-map',
+    entry: ['./app/index.js'],
+
     output: {
         path: './app/',
+        filename: '[name].js',
         publicPath: '/'
     },
 
     module: {
-        loaders: []
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader?presets[]=es2015'
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract('style', 'css!sass')
+            }
+        ]
+    },
+
+    // resolve js and jsx extensions
+    resolve: {
+        extensions: ['', '.js', '.jsx']
     },
 
     plugins: [
@@ -17,5 +36,6 @@ module.exports = {
                 'NODE_ENV': JSON.stringify('production')
             }
         }),
+        new ExtractTextPlugin("[name].css")
     ]
 }
